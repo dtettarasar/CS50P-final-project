9c939b0f300f948421535aca89c6c574fd3751aa
Dylan Tettarasar <dtettarasar@gmail.com>
2025-06-11 10:00:28 +0000

    building test for load img file

--- test_project.py
+++ test_project.py
@@ -2,9 +2,12 @@ from project import load_image_file
 
 def test_load_image_file():
 
-    img_pil = load_image_file("ybear.jpg")
+    img_cs50 = load_image_file("test_files/cs50.jpg")
 
-    ...
+    assert img_cs50.mode == "RGB"
+    assert img_cs50.width == 2048
+    assert img_cs50.height == 1366
+    assert img_cs50.format == "JPEG"
 
 
 def test_function_2():
